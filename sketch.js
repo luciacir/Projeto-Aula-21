3806d0890ef6b6e48ebf42d7dd9a8a9cd731675e
barco <93612733+Profleide@users.noreply.github.com>
2022-11-17 21:45:03 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,33 +3,32 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-var ball, groundObj,leftSide,rightSide;
+
+var ball,groundObj,leftSide,rightSide;
 var world;
-var radius=40;
-function preload()
-{
-	
-}
+var radius = 40;
 
 function setup() {
 	createCanvas(1600, 700);
-rectMode(CENTER);
+	rectMode(CENTER);
 
 	engine = Engine.create();
 	world = engine.world;
 
-	//Create the Bodies Here.
-var ball_options={
-	isStatic:false, 
-restitution:0.3,
-friction:0,
-density:1.2
-}
-ball=Bodies.circle(260,100,radius/2,ball_options);
-World.add(world,ball);
-groundObj=new ground(width/2,670,width,20);
-leftSide=new ground(11000,600,120);
-rightSide=new ground(1350,600,20,120);
+	var ball_options={
+		isStatic:false,
+		restitution:0.3,
+		friction:0,
+		density:1.2
+	}
+
+	ball = Bodies.circle(260,100,radius/2,ball_options);
+	World.add(world,ball);
+
+	groundObj=new ground(width/2,670,width,20);
+	leftSide = new ground(1100,600,20,120);
+	rightSide = new ground(1350,600,20,120);
+
 	Engine.run(engine);
   
 }
@@ -38,20 +37,20 @@ rightSide=new ground(1350,600,20,120);
 function draw() {
   rectMode(CENTER);
   background(0);
-  ellipse (ball.posotion.x,ball.position.y,radius,radius);
+
+
+  ellipse(ball.position.x,ball.position.y,radius,radius);
+
   groundObj.display();
-  leftSite.display();
+  leftSide.display();  
   rightSide.display();
-  drawSprites();
- 
+  
 }
 
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
 
-
-function keyPressed(){
-
-
-if (keyCode===UP_ARROw){
-	Matter.Bodies.applyforce(ball,ball.position,{x:85,w:-85});
-}
+		Matter.Body.applyForce(ball,ball.position,{x:85,y:-85});
+    
+  	}
 }
